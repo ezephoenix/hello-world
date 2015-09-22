a2179ef43c6988435bf440ffa4645d747837b93d
Robert Zhu <robzhu@gmail.com>
2015-09-22 16:16:16 -0400

    serving index.html directly from root path

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 app.use('/static', express.static(__dirname));
+
+app.get('/', function(req, res){
+    fs.readFile("index.html", "utf8", function(err,data){
+        res.send(data);
+    })
+});
 
 app.listen(80, function(){
     console.log('server started on localhost:80');
